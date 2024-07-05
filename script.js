9296c4586b04c587a6288e6dd1b675975534ef7c
Lenício Jr <lenicio.junior@hotmail.com>
2024-07-05 09:33:32 -0300

    Front Resprozante

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    listarPratos();
+})
+
+
+function listarPratos() {
+    fetch('http://127.0.0.1:8080/restaurante/pratos')
+    .then(response => response.json())
+    .then(data => {
+        const pratosTableBody = document.getElementById('pratosTableBody');
+        pratosTableBody.innerHTML = '';
+
+        data.forEach(prato => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${prato.nome}</td>
+                <td>${prato.valor}</td>
+                <td>${prato.tipoPrato}</td>
+            `;
+
+            pratosTableBody.appendChild(row);
+        });
+    })
+}
